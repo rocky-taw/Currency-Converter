4e3c023e403e3d5ef61cf1196fec7816a9fba9d0
Admin <admin@Admins-Mac.local>
2017-09-27 14:31:05 +0600

    This is currency converter in Github repository

--- Currency Converter/ViewController.swift
+++ Currency Converter/ViewController.swift
@@ -8,11 +8,100 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    
+    var myCurrency:[String] = []
+    var myValues:[Double] = []
+    
+    var activeCurrency:Double = 0
+    
+    // Objects
+    @IBOutlet weak var input: UITextField!
+    @IBOutlet weak var pickerView: UIPickerView!
+    @IBOutlet weak var output: UILabel!
+    
+    
+    // Creating Picker view
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return myCurrency.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return myCurrency[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        activeCurrency = myValues[row]
+    }
+    
+    
+    
+    // Button
+    @IBAction func action(_ sender: Any) {
+        
+        if (input.text != "")
+        {
+            output.text = String(Double(input.text!)!*activeCurrency)
+        }
+        
+    }
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+        //Getting Data
+        
+        let url = URL(string: "http://api.fixer.io/latest")
+        
+        let task = URLSession.shared.dataTask(with: url!) { (data, response, error) in
+            
+            if error != nil
+            {
+                 print("ERROR")
+            }
+            
+            else
+            {
+                if let content = data
+                {
+                    do
+                    {
+                        let myJson = try JSONSerialization.jsonObject(with: content, options: JSONSerialization.ReadingOptions.mutableContainers) as AnyObject
+                        
+                        if let rates = myJson["rates"] as? NSDictionary
+                        {
+                            for (key, value) in rates
+                            {
+                                self.myCurrency.append((key as? String)!)
+                                self.myValues.append((value as? Double)!)
+                            }
+                            
+                            
+                        }
+                        
+                    }
+                        
+                    catch
+                    {
+                    
+                    }
+                }
+            }
+            
+            self.pickerView.reloadAllComponents()
+        }
+        
+        task.resume()
     }
 
     override func didReceiveMemoryWarning() {
